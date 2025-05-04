7670a1cac4775e0cbc34120a66305757f796a914
dharun69420 <dharunsr.22aid@kongu.edu>
2025-05-04 21:12:11 +0530

    All files deleted

--- client/src/main.tsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-document.title = "Dhiya Infrastructure | Professional Construction & Infrastructure Services";
-
-createRoot(document.getElementById("root")!).render(<App />);
